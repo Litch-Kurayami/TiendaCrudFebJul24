ea4b86ed6698f6c881192dba0add31cd8354965f
Litch-Kurayami <litchkurayami@gmail.com>
2024-04-13 21:10:20 -0600

    Se han crado los modelos y las tablas necesarias para la migracion de la base de datos [13/04/24 21:10]

---
+++ app/Models/EstadosFacturas.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EstadosFacturas extends Model
+{
+    use HasFactory;
+}
